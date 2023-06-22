805c028106a2c0ba63722a9bd80d216316e827e8
foxws <foxws@users.noreply.github.com>
2023-06-22 13:36:21 +0200

    Clean

--- src/EncryptServiceProvider.php
+++ src/EncryptServiceProvider.php
@@ -2,9 +2,6 @@
 
 namespace Foxws\LivewireEncrypt;
 
-use Foxws\LivewireEncrypt\Middlewares\HydratePublicProperties;
-use Foxws\LivewireEncrypt\Providers\LivewireServiceProvider;
-use Livewire\HydrationMiddleware\HydrationMiddleware;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -18,62 +15,11 @@ class EncryptServiceProvider extends PackageServiceProvider
 
     public function packageRegistered()
     {
-        $this->app->singleton(\Livewire\LivewireManager::class, HydratePublicProperties::class);
-
-        $this->app->bind(
-            \Livewire\LivewireServiceProvider::class,
-            LivewireServiceProvider::class
-        );
-
-        $this->app->bind(
-            HydrationMiddleware::class,
-            HydratePublicProperties::class
-        );
-
-        $this->app->bind(
-            \Livewire\HydrationMiddleware\HydratePublicProperties::class,
-            HydratePublicProperties::class
-        );
-
-        $this->app->singleton(
-            \Livewire\HydrationMiddleware\HydratePublicProperties::class,
-            HydratePublicProperties::class
-        );
+        //
     }
 
     public function packageBooted(): void
     {
-        // $this->app->singleton(\Livewire\LivewireServiceProvider::class, LivewireServiceProvider::class);
-
-        $this->app->bind(
-            \Livewire\LivewireServiceProvider::class,
-            LivewireServiceProvider::class
-        );
-
-        $this->app->bind(
-            HydrationMiddleware::class,
-            HydratePublicProperties::class
-        );
-
-        $this->app->singleton(
-            HydrationMiddleware::class,
-            HydratePublicProperties::class
-        );
-
-        $this->app->bind(
-            \Livewire\HydrationMiddleware\HydratePublicProperties::class,
-            HydratePublicProperties::class
-        );
-
-        $this->app->singleton(
-            \Livewire\HydrationMiddleware\HydratePublicProperties::class,
-            HydratePublicProperties::class
-        );
-
-        // $loader = \Illuminate\Foundation\AliasLoader::getInstance();
-        // $loader->alias(
-        //     \Livewire\HydrationMiddleware\HydratePublicProperties::class,
-        //     HydratePublicProperties::class
-        // );
+        //
     }
 }
